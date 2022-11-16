72a980fc9049ab797ec07d08c730a2619b9d163f
mr-Freeman-S <mr.baranok.slava@gmail.com>
2022-11-16 13:27:08 +0300

    Easy balace checking solved k6

--- EasyBalanceChecking.js
+++ EasyBalanceChecking.js
@@ -2,7 +2,6 @@ function balance(book) {
     const regex = /[a-zA-Z /\d/g/./]+/g
     // your code
     // Please use .toFixed(2) to format numbers
-<<<<<<< HEAD
     let bookToArray = book.match(regex).join(' ').split(' ').filter(el => el!==' '&&el!=='' )
     let currentMoney = Number(bookToArray[0]).toFixed(2)
     let answer = `Original Balance: ${currentMoney}\r\n`
@@ -15,12 +14,12 @@ function balance(book) {
             totalExpense += +element
             totalOpeations++
         } else if((i % 3 === 1) ){
-            answer += `  ${element}`
+            answer += `${element}`
         } else{
             answer += ` ${element}`
         }
     }
-    return answer + `Total expense  ${totalExpense.toFixed(2)}\r\nAverage expense  ${(totalExpense / totalOpeations).toFixed(2)}`
+    return answer + `  Total expense  ${totalExpense.toFixed(2)}\r\n  Average expense  ${(totalExpense / totalOpeations).toFixed(2)}`
 }
 
 
@@ -73,38 +72,3 @@ function balance(book) {
   Average expense  53.41`
 
 console.log(balance(b2))
-=======
-    let bookToArray = book.match(regex).filter(el => el!==' ').join('')
-    let currentMoney = bookToArray.shift()
-    let answer = `Original Balance ${currentMoney}\r\n`
-    let totalExpense = 0
-    for (let i = 0; i < bookToArray.length; i++) {
-        const element = bookToArray[i];
-        if (i % 2 === 1) {
-            answer += `${bookToArray[i]} Balance: ${(currentMoney -= (+element)).toFixed(2)}\r\n`
-            totalExpense += +element
-        } else {
-            answer += element
-
-        }
-    }
-    console.log(answer)
-    return answer + `Total expense  ${totalExpense.toFixed(2)}\r\nAverage expense  ${(totalExpense / ((bookToArray.length) / 2)).toFixed(2)}`
-}
-
-const input = `1233.00
-125 Hardware;! 24.8?;
-123 Flowers 93.5
-127 Meat 120.90
-120 Picture 34.00
-124 Gasoline 11.00
-123 Photos;! 71.4?;
-122 Picture 93.5
-132 Tyres;! 19.00,?;
-129 Stamps 13.6
-129 Fruits{} 17.6
-129 Market;! 128.00?;
-121 Gasoline;! 13.6?;`
-console.log(balance(input));
-
->>>>>>> 3a12fe7eb2ace1ae1cdfc82202af1395d2d9123d
--- test.js
+++ test.js
@@ -1,12 +1,7 @@
-function digital_root(n) {
-    let str = String(n)
-    let answer = 0
-    while (str.length > 1) {
-        for (let i = 0; i < str.length; i++) {
-          const element = str[i];
-          answer += element
-        }
-        str = String(answer)
-    }
-    return answer
-  }
+
+const { default: test } = require("node:test");
+
+
+test('EasyBalanceChecking',()=> {
+  expect(balance())
+})
